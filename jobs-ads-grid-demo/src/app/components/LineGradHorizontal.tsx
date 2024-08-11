03d9e4bb4e51d35f5ed131c962628b1b57858485
Iftekhar16 <iftekharhossain16@gmail.com>
2024-08-11 16:34:54 +0600

    feat(jobs-list-page): created the jobs list page

---
+++ jobs-ads-grid-demo/src/app/components/LineGradHorizontal.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LineGradHorizontal = () => {
+    return (
+        <div className="line-grad-horizontal bg-gradient-to-r from-[#7932B0] to-[#3A28B9] flex-grow h-[1px]"></div>
+    );
+};
+
+export default LineGradHorizontal;
---
+++ jobs-ads-grid-demo/src/app/components/LineGradVertical.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LineGradVertical = () => {
+    return (
+        <div className="line-grad-vertical bg-gradient-to-r from-[#7932B0] to-[#3A28B9] w-[1px] h-full"></div>
+    );
+};
+
+export default LineGradVertical;
